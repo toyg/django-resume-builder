7af7c063d9550906275e84ce6850f425e5913028
toyg <g.lacava@gmail.com>
2019-07-02 17:44:05 +0100

    Adding admin interface for Resume and ResumeItemLink models

--- apps/resume/admin.py
+++ apps/resume/admin.py
@@ -7,3 +7,16 @@ from . import models
 class ResumeItemAdmin(admin.ModelAdmin):
     list_display = ('user', 'title', 'company', 'start_date')
     ordering = ('user', '-start_date')
+
+
+class ResumeItemLinkInline(admin.StackedInline):
+    model = models.ResumeItemLink
+    ordering = ('order',)
+    extra = 1
+
+
+@admin.register(models.Resume)
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'label')
+    ordering = ('user', '-updated_on')
+    inlines = [ResumeItemLinkInline]
